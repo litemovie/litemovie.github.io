66e220aab80e33a3d43d1083e7f6be39686ceeac
litemovie <109190218+litemovie@users.noreply.github.com>
2022-07-19 15:19:07 +0000

    Create js.js

---
+++ mov/an/js/js.js
@@ -0,0 +1,25 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function one() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function two() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
